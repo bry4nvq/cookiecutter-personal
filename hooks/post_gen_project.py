4eb4e8b68b205f4761cadcece9fa47f5c12b4c15
bry4nvq <119910030+bry4nvq@users.noreply.github.com>
2023-12-23 19:37:31 -0500

    Commenting the env installing

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,10 +8,10 @@ RESET_ALL = "\x1b[0m"
 
 print(f"{MESSAGE_COLOR}Almost done!")
 
-print(f"{MESSAGE_COLOR}Installing environment...{RESET_ALL}")
-subprocess.call(['mamba', 'env', 'create','--file','environment.yml'])
-subprocess.call(['mamba', 'activate', project_slug])
-subprocess.call(['mamba', 'list'])
+#print(f"{MESSAGE_COLOR}Installing environment...{RESET_ALL}")
+#subprocess.call(['mamba', 'env', 'create','--file','environment.yml'])
+#subprocess.call(['mamba', 'activate', project_slug])
+#subprocess.call(['mamba', 'list'])
 
 print(f"{MESSAGE_COLOR}Initializing a git repository...{RESET_ALL}")
 
